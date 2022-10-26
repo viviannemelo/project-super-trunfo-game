5979280e5d19ec921d061c19ff8fc17be01ac36e
viviannemelo <viviannemmelo@gmail.com>
2022-10-26 18:17:56 -0300

    Card: propTypes das props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -38,4 +38,15 @@ class Card extends Component {
   }
 }
 
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
+
 export default Card;
